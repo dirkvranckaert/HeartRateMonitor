04e92fd9d38083047caa43c6d30e22e4ddbc0caa
Dirk Vranckaert <dirkvranckaert@gmail.com>
2016-04-22 23:05:44 +0200

    Formatting

--- shared/src/main/java/eu/vranckaert/heart/rate/monitor/shared/AbstractHeartRateApplication.java
+++ shared/src/main/java/eu/vranckaert/heart/rate/monitor/shared/AbstractHeartRateApplication.java
@@ -5,23 +5,23 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 
-/**
- * Date: 22/02/16
- * Time: 22:17
- *
- * @author Dirk Vranckaert
- */
-public class AbstractHeartRateApplication extends Application {
-    private static final Handler mHandler = new Handler();
+    /**
+     * Date: 22/02/16
+     * Time: 22:17
+     *
+     * @author Dirk Vranckaert
+     */
+    public class AbstractHeartRateApplication extends Application {
+        private static final Handler mHandler = new Handler();
 
-    public static final void runOnUiThread(Runnable runnable) {
-        Log.d(AbstractHeartRateApplication.class.getSimpleName(), "runOnUiThread");
-        if (Thread.currentThread() == Looper.getMainLooper().getThread()) {
-            Log.d(AbstractHeartRateApplication.class.getSimpleName(), "runnable.run()");
-            runnable.run();
-        } else {
-            Log.d(AbstractHeartRateApplication.class.getSimpleName(), "handler.post()");
-            mHandler.post(runnable);
+        public static final void runOnUiThread(Runnable runnable) {
+            Log.d(AbstractHeartRateApplication.class.getSimpleName(), "runOnUiThread");
+            if (Thread.currentThread() == Looper.getMainLooper().getThread()) {
+                Log.d(AbstractHeartRateApplication.class.getSimpleName(), "runnable.run()");
+                runnable.run();
+            } else {
+                Log.d(AbstractHeartRateApplication.class.getSimpleName(), "handler.post()");
+                mHandler.post(runnable);
+            }
         }
     }
-}
